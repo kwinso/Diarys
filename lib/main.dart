612ed54735d46e2cf1fa2fd2d4e46a8ef105c58f
uwumouse <pythonisajoke@gmail.com>
2022-02-19 23:53:24 +0300

    Remove some boilerplate from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const MainPage(title: "Diarys"),
+      home: const MainPage(),
       title: "Diarys",
       theme: AppThemeData.light,
       darkTheme: AppThemeData.dark,
@@ -39,18 +39,9 @@ class _AppState extends State<App> {
 }
 
 class MainPage extends StatefulWidget {
-  const MainPage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MainPage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<MainPage> createState() => _MainPageState();
@@ -74,12 +65,6 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: const CustomAppBar(),
       body: screens[activeScreen],
