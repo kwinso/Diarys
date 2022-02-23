c149ff1a92c348891d41ac13aa08a713b00c66a3
uwumouse <pythonisajoke@gmail.com>
2022-02-23 10:56:22 +0300

    Editiing mode base.

---
+++ lib/components/schedule/editing_mode_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// TODO
+class EditModeScheduleList extends StatefulWidget {
+  EditModeScheduleList({Key? key}) : super(key: key);
+
+  @override
+  State<EditModeScheduleList> createState() => _EditModeScheduleListState();
+}
+
+class _EditModeScheduleListState extends State<EditModeScheduleList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
